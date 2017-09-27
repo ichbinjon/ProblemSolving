b4df66d3eafd13b0fb363d53fc3425c54cd0343f
ichbinjon <jpgmend@gmail.com>
2017-09-27 17:02:27 +0100

    hackerrank

---
+++ ArraysLeftPosition.py
@@ -0,0 +1,13 @@
+#https://www.hackerrank.com/challenges/ctci-array-left-rotation
+
+def array_left_rotation(a, n, k):
+      for x in range(0,k,1):
+        temp = a.pop(0)
+        a.append(temp)  
+      return a
+
+n, k = map(int, input().strip().split(' '))
+a = list(map(int, input().strip().split(' ')))
+
+answer = array_left_rotation(a, n, k);
+print(*answer, sep=' ')
