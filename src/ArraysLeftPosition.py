14870b91ec5f1b7df8a516399429c475011f8178
ichbinjon <jpgmend@gmail.com>
2018-02-09 10:32:20 +0000

    added solution with list slicing

--- src/ArraysLeftPosition.py
+++ src/ArraysLeftPosition.py
@@ -1,13 +1,21 @@
-#https://www.hackerrank.com/challenges/ctci-array-left-rotation
-# Space O(1) Computation O(n)
+# https://www.hackerrank.com/challenges/ctci-array-left-rotation
+
+
 def array_left_rotation(a, n, k):
-      for x in range(0,k,1):
+    # Space O(1) Computation O(n) -> actually O(n*k)
+    for x in range(0, k, 1):
         temp = a.pop(0)
-        a.append(temp)  
-      return a
+        a.append(temp)
+    return a
+
+
+def array_left_rotation_2(a, n, k):
+    # another solution using list slicing
+        return a[k:]+a[:k]
+
 
 n, k = map(int, input().strip().split(' '))
 a = list(map(int, input().strip().split(' ')))
 
-answer = array_left_rotation(a, n, k);
+answer = array_left_rotation(a, n, k)
 print(*answer, sep=' ')
